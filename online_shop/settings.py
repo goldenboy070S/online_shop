ac43925ec60db485cbafaee4510048a737e19f7a
goldenboy070S <sr080hz@gmail.com>
2024-11-29 13:51:03 +0500

    Update settings.py for production

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -51,9 +51,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissions',
     ),
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework.authentication.TokenAuthentication',
-    # ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
